6e6f90e942307211bd3a187e358df22fa26699dd
jacquelineturof <jacquelineturof@gmail.com>
2020-09-13 08:55:48 -0400

    forgot json parse

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 var admin = require('firebase-admin');
 
 admin.initializeApp({
-  credential: admin.credential.cert(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii')),
+  credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.GOOGLE_CONFIG_BASE64, 'base64').toString('ascii'))),
   databaseURL: "https://court-queue-prototype.firebaseio.com"
 });
 
